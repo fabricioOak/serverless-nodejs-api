21d2659d7ae5e9da6a2c30a56d19de142325bf1b
Fabricio Carvalho <fcarvalho1999@gmail.com>
2024-03-05 15:58:03 -0300

    feat(index.js): add support for fetching data from a Neon database using @neondatabase/serverless package feat(index.js): add async function dbClient to handle database connection and query execution feat(index.js): modify / route to fetch and return results from a database query using Neon database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const serverless = require("serverless-http");
 const express = require("express");
+const { neon, neonConfig } = require("@neondatabase/serverless");
 const app = express();
 
-app.get("/", (req, res, next) => {
+async function dbClient() {
+	neonConfig.fetchConnectionCache = true;
+	return (sql = neon(process.env.DATABASE_URL));
+}
+
+app.get("/", async (req, res, next) => {
+	const db = await dbClient();
+	const results = await db`select now()`;
 	return res.status(200).json({
 		message: "Hello from root!",
 		DATABASE_URL: process.env.DATABASE_URL,
+		results,
 	});
 });
 
